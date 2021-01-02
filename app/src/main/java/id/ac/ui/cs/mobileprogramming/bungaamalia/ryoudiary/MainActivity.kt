0aec7dd7f5c913b8ca9701f7db1556c1305b6320
Bunga Amalia <bungasokia@gmail.com>
2021-01-02 11:56:29 +0700

    Move recipe list to recipe activity

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/bungaamalia/ryoudiary/MainActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/bungaamalia/ryoudiary/MainActivity.kt
@@ -7,28 +7,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
-import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.observe
 import androidx.navigation.findNavController
 import androidx.navigation.ui.onNavDestinationSelected
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import id.ac.ui.cs.mobileprogramming.bungaamalia.ryoudiary.adapter.RecipeListAdapter
-import id.ac.ui.cs.mobileprogramming.bungaamalia.ryoudiary.application.RecipesApplication
-import id.ac.ui.cs.mobileprogramming.bungaamalia.ryoudiary.data.Recipe
 import id.ac.ui.cs.mobileprogramming.bungaamalia.ryoudiary.databinding.ActivityMainBinding
-import id.ac.ui.cs.mobileprogramming.bungaamalia.ryoudiary.viewmodel.RecipeViewModel
-import id.ac.ui.cs.mobileprogramming.bungaamalia.ryoudiary.viewmodel.RecipeViewModelFactory
 
 class MainActivity : AppCompatActivity() {
     private val delayExit = 2000
     private var backPressedTimer: Long = 0
     private lateinit var binding: ActivityMainBinding
-    private val recipeActivityRequestCode = 1
-    private val recipeViewModel: RecipeViewModel by viewModels {
-        RecipeViewModelFactory((application as RecipesApplication).repository)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,38 +23,6 @@ class MainActivity : AppCompatActivity() {
             R.layout.activity_main
         )
         setSupportActionBar(binding.toolbar)
-
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
-        val adapter = RecipeListAdapter()
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(this)
-
-        recipeViewModel.allRecipes.observe(owner = this) { recipes ->
-            recipes.let { adapter.submitList(it) }
-        }
-
-        binding.fab.setOnClickListener {
-            val intent = Intent(this@MainActivity, RecipeActivity::class.java)
-            startActivityForResult(intent, recipeActivityRequestCode)
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, intentData: Intent?) {
-        super.onActivityResult(requestCode, resultCode, intentData)
-
-        if (requestCode == recipeActivityRequestCode && resultCode == Activity.RESULT_OK) {
-            intentData?.getStringExtra(RecipeActivity.EXTRA_REPLY)?.let { reply ->
-                val recipe = Recipe(0, reply, "test desc",
-                    "test image uri string", "test date")
-                recipeViewModel.insertRecipe(recipe)
-            }
-        } else {
-            Toast.makeText(
-                applicationContext,
-                R.string.recipe_empty_not_saved,
-                Toast.LENGTH_LONG
-            ).show()
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
--- app/src/main/java/id/ac/ui/cs/mobileprogramming/bungaamalia/ryoudiary/RecipeActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/bungaamalia/ryoudiary/RecipeActivity.kt
@@ -7,17 +7,51 @@ import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.observe
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import id.ac.ui.cs.mobileprogramming.bungaamalia.ryoudiary.adapter.RecipeListAdapter
+import id.ac.ui.cs.mobileprogramming.bungaamalia.ryoudiary.application.RecipesApplication
+import id.ac.ui.cs.mobileprogramming.bungaamalia.ryoudiary.data.Recipe
+import id.ac.ui.cs.mobileprogramming.bungaamalia.ryoudiary.databinding.FragmentRecipeListBinding
+import id.ac.ui.cs.mobileprogramming.bungaamalia.ryoudiary.viewmodel.RecipeViewModel
+import id.ac.ui.cs.mobileprogramming.bungaamalia.ryoudiary.viewmodel.RecipeViewModelFactory
 
 class RecipeActivity : AppCompatActivity() {
     private lateinit var editRecipeName: EditText
+    private lateinit var binding: FragmentRecipeListBinding
+    private val recipeActivityRequestCode = 1
+    private val recipeViewModel: RecipeViewModel by viewModels {
+        RecipeViewModelFactory((application as RecipesApplication).repository)
+    }
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,
+            R.layout.fragment_recipe_list
+        )
         setContentView(R.layout.activity_recipe)
         editRecipeName = findViewById(R.id.edit_recipe_name)
 
-        val button = findViewById<Button>(R.id.recipe_button_save)
-        button.setOnClickListener {
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val adapter = RecipeListAdapter()
+        recyclerView.adapter = adapter
+
+        recipeViewModel.allRecipes.observe(owner = this) { recipes ->
+            recipes.let { adapter.submitList(it) }
+        }
+
+        binding.fab.setOnClickListener {
+            val intent = Intent(this@RecipeActivity, RecipeActivity::class.java)
+            startActivityForResult(intent, recipeActivityRequestCode)
+        }
+
+        val saveButton = findViewById<Button>(R.id.recipe_button_save)
+        saveButton.setOnClickListener {
             val replyIntent = Intent()
             if (TextUtils.isEmpty(editRecipeName.text)) {
                 setResult(Activity.RESULT_CANCELED, replyIntent)
@@ -27,6 +61,24 @@ class RecipeActivity : AppCompatActivity() {
                 setResult(Activity.RESULT_OK, replyIntent)
             }
             finish()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intentData: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intentData)
+
+        if (requestCode == recipeActivityRequestCode && resultCode == Activity.RESULT_OK) {
+            intentData?.getStringExtra(RecipeActivity.EXTRA_REPLY)?.let { reply ->
+                val recipe = Recipe(0, reply, "test desc",
+                    "test image uri string", "test date")
+                recipeViewModel.insertRecipe(recipe)
+            }
+        } else {
+            Toast.makeText(
+                applicationContext,
+                R.string.recipe_empty_not_saved,
+                Toast.LENGTH_LONG
+            ).show()
         }
     }
 
